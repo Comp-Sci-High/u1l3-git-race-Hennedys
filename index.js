ae0b4425a0fcd38877cf0c53a8a8cb4ee047c81e
Hennedys <hennedys.guerra26@compscihigh.org>
2024-09-18 15:13:29 +0000

    Changes Made In both Index.js and more.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Task 1
 // Print your name
-
+console.log("Hennedys Guerra")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("Pringles")
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+console.log("Romeo Santos")
 // Stage and commit this change with an appropriate message
 // Go to more.js
--- more.js
+++ more.js
@@ -1,9 +1,9 @@
 // Task 4
 // Print your favorite food space
-
+console.log("Taco Bell4")
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+console.log("Philly Cheesesteak and a Arizona.")
 // Stage and commit this change with an appropriate message
 // Go to README.md
